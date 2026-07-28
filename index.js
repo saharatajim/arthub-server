15f5e046eb255fd875f6fb7a6c2a3abe591fdb9a
saharatajim <sahara.tajim@gmail.com>
2026-07-29 04:42:41 +0600

    deploy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors=require("cors")
 const app = express()              
 app.use(cors())   
 app.use(express.json()) 
-const port = 5000    
+const port = process.env.PORT   
 
 app.get('/', (req, res) => {
   res.send('Hello World! Server is running successfully 🚀')
